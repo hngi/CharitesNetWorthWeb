67b80c726368a074b5d330bc554866914369ea7f
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-09-29 11:36:20 +0100

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -1,12 +1,14 @@
 <?php
-  $name = $_POST['contact_name'];
-  $email = $_POST['contact_email'];
-  $phone = $_POST['contact_number'];
-  $message = $_POST['contact_message'];
-  $formcontent="From: $name \n $phone \n Message: $message";
-  $recipient = "teamcharites@gmail.com";
-  $subject = "Contact Form";
-  $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-  echo "Thank you for contacting CharitesFinance!";
+if(isset($_POST['contact_submit'])) {
+    $name = $_POST['contact_name'];
+    $email = $_POST['contact_email'];
+    $phone = $_POST['contact_number'];
+    $message = $_POST['contact_message'];
+    $formcontent="From: $name \n $phone \n Message: $message";
+    $recipient = "teamcharites@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank you for contacting CharitesFinance!";
+    }
 ?>

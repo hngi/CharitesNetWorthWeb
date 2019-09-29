c7beb7a9634ef49f31e46318be8b87b619ed7a1d
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-09-29 16:27:35 +0100

    Trying signup

--- includes/config.php
+++ includes/config.php
@@ -4,8 +4,3 @@ $databaseHost = 'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306'
 $databaseName = 'xsv1qjd2he2pzjgh';
 $databaseUser = 'ze8g19ard2iom13t';
 $databasePassword = 'er7h7x0psn0ycdfa';
-//Implementation Type
-//choose between `DATABASE` or `FILE`
-$implementationType = 'FILE';
-//Website
-$baseUrl = 'https://charites-networthapp.herokuapp.com';

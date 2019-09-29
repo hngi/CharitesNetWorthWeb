8b217aa1b7adae6be92689911f740d609d0cdd21
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-09-29 11:15:30 +0100

    Included mobile number on delivery

--- contact_mail.php
+++ contact_mail.php
@@ -3,7 +3,7 @@
   $email = $_POST['contact_email'];
   $phone = $_POST['contact_number'];
   $message = $_POST['contact_message'];
-  $formcontent="From: $name \n Message: $message";
+  $formcontent="From: $name \n $phone \n Message: $message";
   $recipient = "teamcharites@gmail.com";
   $subject = "Contact Form";
   $mailheader = "From: $email \r\n";

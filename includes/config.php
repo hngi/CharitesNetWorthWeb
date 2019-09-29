2bc617713d95349879bfd50d2c0c6799124bf10f
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-09-29 16:37:36 +0100

    change

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,9 @@
 <?php
+
 //Database Options
-$databaseHost = 'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
-$databaseName = 'xsv1qjd2he2pzjgh';
-$databaseUser = 'ze8g19ard2iom13t';
-$databasePassword = 'er7h7x0psn0ycdfa';
+$databaseHost = 'localhost';
+$databaseName = 'teamcharites';
+$databaseUser = 'root';
+$databasePassword = '';
+//Website
+$baseUrl = 'https://charites-networthapp.herokuapp.com'

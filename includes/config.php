3f7a4f77e9daad99367213a8a3f10e4b5ba22dcf
TeamCharites <55658839+TeamCharites@users.noreply.github.com>
2019-09-28 23:37:55 -0700

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+//Database Options
+$databaseHost = 'bmsyhziszmhf61g1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306';
+$databaseName = 'xsv1qjd2he2pzjgh';
+$databaseUser = 'ze8g19ard2iom13t';
+$databasePassword = 'er7h7x0psn0ycdfa';
+//Implementation Type
+//choose between `DATABASE` or `FILE`
+$implementationType = 'FILE';
+//Website
+$baseUrl = 'https://charites-networthapp.herokuapp.com';

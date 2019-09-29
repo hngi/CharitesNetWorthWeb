b61a5203c7541b481c56e9f6cf0a4d2fea38a859
TeamCharites <55658839+TeamCharites@users.noreply.github.com>
2019-09-29 00:39:28 -0700

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,25 +1,47 @@
 <?php
-	function validateFormData($data){
-	$data = trim($data);
-	$data = stripslashes($data);
-	$data = htmlspecialchars($data);
-	$data = strip_tags($data);
-	
-	return $data;
-}
-function mytoken($len = 10){
-	$token = "k346vbmzx5780f01asdkhlkiutyroepwqplkjhgfds36vxmc";
-	$token = str_shuffle($token);
-	$token = substr($token, 0, $len);
-	
-	return $token;
-}
-function redirectToPass(){
-	header("Location: forgotpassword.php");
-	exit();
-}
-function redirectToLogin(){
-	header("Location: signup.html");
-	exit();
-}
+	function validateEmail($email) {
+		return (!preg_match(
+			"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^",
+			$email
+		)) ? FALSE : TRUE;
+	}
+	//Show Alert function
+	function showAlert() {
+		if (isset($_SESSION['alert_flag']) && isset($_SESSION['alert_message'])) {
+			if ($_SESSION['alert_flag'] == 'error') { 
+				//Add CSS & FrontEnd to display Errors here -->
+				// data-source attribute will indicate in dev-tools that this was php inserted.
+				//Do this by exposing: -->
+				echo "<div data-source='php' class='alert alert-danger'>" . $_SESSION['alert_message'] . "</div>";
+			} 
+			elseif ($_SESSION['alert_flag'] == 'success') { 
+				// Add CSS & FrontEnd to display Success Messages here -->
+				// data-source attribute will indicate in dev-tools that this was php inserted.
+				// Do this by exposing: -->
+				echo "<div data-source='php' class='alert alert-success'>" . $_SESSION['alert_message'] . "</div>";
+			}
+			elseif ($_SESSION['alert_flag'] == 'info') {
+				// Add CSS & FrontEnd to display Notices here -->
+				// data-source attribute will indicate in dev-tools that this was php inserted.
+				// Do this by exposing: -->
+				echo "<div data-source='php' class='alert alert-info'>" . $_SESSION['alert_message'] . "</div>";
+			}
+			 
+			//DON'T EDIT!!!
+			unset($_SESSION['alert_flag']);
+			unset($_SESSION['alert_message']);
+		}
+	}
+	//Add Alert function
+	function addAlert($flag, $message) {
+		//PLEASE, DON'T EDIT!!!
+		if ($flag !== '' && $message !== '') {
+			$_SESSION['alert_flag'] = $flag;
+			$_SESSION['alert_message'] = $message;
+		} 
+		else {
+			unset($_SESSION['alert_flag']);
+			unset($_SESSION['alert_message']);
+		}
+	}
 ?>

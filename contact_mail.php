54ce33486b1858641447e3221fe01fc03b11d01a
Joseph Etim <54282162+pepjoe@users.noreply.github.com>
2019-09-29 11:48:20 +0100

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -1,14 +1,20 @@
-<?php
-if(isset($_POST['contact_submit'])) {
-    $name = $_POST['contact_name'];
-    $email = $_POST['contact_email'];
-    $phone = $_POST['contact_number'];
-    $message = $_POST['contact_message'];
-    $formcontent="From: $name \n $phone \n Message: $message";
-    $recipient = "teamcharites@gmail.com";
-    $subject = "Contact Form";
-    $mailheader = "From: $email \r\n";
-    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-    echo "Thank you for contacting CharitesFinance!";
+<?php 
+if(isset($_POST['submit'])){
+    $to = "teamcharites@gmail.com";
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $phone = $_POST['number'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name . " " . $number . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
     }
 ?>

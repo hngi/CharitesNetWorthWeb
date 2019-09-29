78ac044ef4ca0d916ab14287c6b18aa4b7c564a0
TeamCharites <55658839+TeamCharites@users.noreply.github.com>
2019-09-29 13:30:01 -0700

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,9 +1,13 @@
 <?php
+
 require_once('config.php');
+
 // $con = mysqli_connect("localhost", "root", "", "teamcharites");
 $con = mysqli_connect($databaseHost, $databaseUser, $databasePassword, $databaseName);
+
 // Check connection
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+
 ?>
